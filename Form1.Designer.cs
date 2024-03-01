6c6c711552bb4aa4394457135ed697f26a59b940
akinaw2133 <161851741+akinaw2133@users.noreply.github.com>
2024-03-01 17:29:42 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SID = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(46, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "File Path";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(153, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "directory path....";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(252, 342);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(65, 21);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Upload ";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowDrop = true;
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle4;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle5.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle5.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle5.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle5;
+            this.dataGridView1.Location = new System.Drawing.Point(202, 51);
+            this.dataGridView1.Name = "dataGridView1";
+            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle6.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle6.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle6.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle6.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle6;
+            this.dataGridView1.Size = new System.Drawing.Size(546, 256);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(376, 339);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Show";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(505, 340);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Update";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(639, 339);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "Delete";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(96, 215);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(86, 70);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 10;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(86, 113);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 11;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(86, 160);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 12;
+            // 
+            // SID
+            // 
+            this.SID.AutoSize = true;
+            this.SID.Location = new System.Drawing.Point(32, 77);
+            this.SID.Name = "SID";
+            this.SID.Size = new System.Drawing.Size(26, 13);
+            this.SID.TabIndex = 13;
+            this.SID.Text = "Age";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(32, 120);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Name";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(19, 163);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Location";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(827, 394);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.SID);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label SID;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,350 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Excel;
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection con = new SqlConnection("Data Source=DESKTOP-8J0J8M8\\sqlexpress;Initial Catalog=master;Integrated Security=True");
+
+        OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            DialogResult result = openFileDialog1.ShowDialog();
+
+            try
+            {
+                // Create OpenFileDialog
+
+                // Set filter for file extension and default file extension
+                openFileDialog1.Filter = "Excel Files|*.xls;*.xlsx;*.xlsm";
+                openFileDialog1.FilterIndex = 1;
+
+                // Display OpenFileDialog by calling ShowDialog method
+                // Check if the user selected a file
+                if (result == DialogResult.OK)
+                {
+                    string filename = openFileDialog1.FileName;
+                    if (filename == null)
+                    {
+                        MessageBox.Show("Please select a valid document.");
+                    }
+                    else
+                    {
+                        try
+                        {
+                            // Open connection
+                            con.Open();
+
+                            // Excel Application
+                            Microsoft.Office.Interop.Excel.Application excelApp = new Microsoft.Office.Interop.Excel.Application();
+                            // Open the Workbook
+                            Workbook workbook = excelApp.Workbooks.Open(filename);
+
+                            // Select the first worksheet
+                            Worksheet worksheet = (Worksheet)workbook.Sheets[1];
+
+                            // Range of used cells
+                            Range range = worksheet.UsedRange;
+
+                            // Row count and column count
+                            int rowCount = range.Rows.Count;
+                            int columnCount = range.Columns.Count;
+
+                            // Loop through the rows in the Excel sheet
+                            for (int row = 2; row <= rowCount; row++) // Assuming the first row contains headers
+                            {
+                                // Insert command with parameters
+                                string insertQuery = "INSERT INTO doc (SID, NAME, AGE, LOCATION) VALUES (@SID, @NAME, @AGE, @LOCATION)";
+                                SqlCommand cmd = new SqlCommand(insertQuery, con);
+
+                                // Add parameter values from Excel
+                                cmd.Parameters.AddWithValue("@SID", ((Range)range.Cells[row, 1]).Value2);
+                                cmd.Parameters.AddWithValue("@NAME", ((Range)range.Cells[row, 2]).Value2);
+                                cmd.Parameters.AddWithValue("@AGE", ((Range)range.Cells[row, 3]).Value2);
+                                cmd.Parameters.AddWithValue("@LOCATION", ((Range)range.Cells[row, 4]).Value2);
+
+                                // Execute the command
+                                cmd.ExecuteNonQuery();
+                            }
+
+                            MessageBox.Show("Data uploaded successfully.");
+
+                            // Close the workbook and quit Excel
+                            workbook.Close();
+                            excelApp.Quit();
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("Data Already uploaded ");
+                        }
+                        finally
+                        {
+                            // Close connection
+                            con.Close();
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                // Open the connection to your database
+                con.Open();
+
+                // SQL query to select data from the "doc" table
+                string query = "SELECT * FROM doc";
+
+                // Create a SqlCommand object
+                using (SqlCommand cmd = new SqlCommand(query, con))
+                {
+                    // Create a SqlDataAdapter to fetch data
+                    SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+
+                    // Create a DataTable to store the data
+                    System.Data.DataTable table = new System.Data.DataTable();
+
+                    // Fill the DataTable with data from the adapter
+                    adapter.Fill(table);
+
+                    // Display data in the DataGridView
+                    dataGridView1.DataSource = table;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+            finally
+            {
+                // Close the connection to your database
+                con.Close();
+            }
+
+        }
+        // Excel Application object
+        System.Data.DataTable excelApp = new System.Data.DataTable();
+        // List to store sheet names
+        List<string> sheetNames = new List<string>();
+
+
+
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                // Create OpenFileDialog
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+
+                // Set filter for file extension and default file extension
+                openFileDialog.Filter = "Excel Files|*.xls;*.xlsx;*.xlsm";
+                openFileDialog.FilterIndex = 1;
+
+                // Display OpenFileDialog by calling ShowDialog method
+                DialogResult result = openFileDialog.ShowDialog();
+
+                // Check if the user selected a file
+                if (result == DialogResult.OK)
+                {
+                    string filename = openFileDialog.FileName;
+                    if (filename == null)
+                    {
+                        MessageBox.Show("Please select a valid document.");
+                    }
+                    else
+                    {
+                        try
+                        {
+                            // Open connection
+                            con.Open();
+
+                            // Excel Application
+                            Microsoft.Office.Interop.Excel.Application excelApp = new Microsoft.Office.Interop.Excel.Application();
+                            // Open the Workbook
+                            Workbook workbook = excelApp.Workbooks.Open(filename);
+
+                            // Select the first worksheet
+                            Worksheet worksheet = (Worksheet)workbook.Sheets[1];
+
+                            // Range of used cells
+                            Range range = worksheet.UsedRange;
+
+                            // Row count and column count
+                            int rowCount = range.Rows.Count;
+                            int columnCount = range.Columns.Count;
+
+                            // Loop through the rows in the Excel sheet
+                            for (int row = 2; row <= rowCount; row++) // Assuming the first row contains headers
+                            {
+                                // Insert command with parameters
+                                string insertQuery = "INSERT INTO doc (SID, NAME, AGE, LOCATION) VALUES (@SID, @NAME, @AGE, @LOCATION)";
+                                SqlCommand cmd = new SqlCommand(insertQuery, con);
+
+                                // Add parameter values from Excel
+                                cmd.Parameters.AddWithValue("@SID", ((Range)range.Cells[row, 1]).Value2);
+                                cmd.Parameters.AddWithValue("@NAME", ((Range)range.Cells[row, 2]).Value2);
+                                cmd.Parameters.AddWithValue("@AGE", ((Range)range.Cells[row, 3]).Value2);
+                                cmd.Parameters.AddWithValue("@LOCATION", ((Range)range.Cells[row, 4]).Value2);
+
+                                // Execute the command
+                                cmd.ExecuteNonQuery();
+                            }
+
+                            MessageBox.Show("Data Updated successfully.");
+
+                            // Close the workbook and quit Excel
+                            workbook.Close();
+                            excelApp.Quit();
+                        }
+                        catch (Exception ex)
+                        {
+                            //MessageBox.Show("Data Updated uploaded ");
+                        }
+                        finally
+                        {
+                            // Close connection
+                            con.Close();
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Open the SqlConnection
+                con.Open();
+
+                // SQL query to delete uploaded data
+                string query = "DELETE FROM doc";
+
+                // Create a SqlCommand object
+                using (SqlCommand cmd = new SqlCommand(query, con))
+                {
+                    // Execute the command
+                    int rowsAffected = cmd.ExecuteNonQuery();
+
+                    // Show a message box indicating the number of rows deleted
+                    MessageBox.Show(rowsAffected + " row(s) deleted successfully.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+            finally
+            {
+                // Close the SqlConnection
+                con.Close();
+            }
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Open the database connection
+                con.Open();
+
+                // Get the search conditions from the TextBoxes
+                string ageCondition = textBox1.Text.Trim();
+                string nameCondition = textBox2.Text.Trim();
+                string locationCondition = textBox3.Text.Trim();
+
+                // SQL query to select data based on all the conditions
+                string query = "SELECT * FROM doc WHERE 1 = 1";
+
+                if (!string.IsNullOrEmpty(ageCondition))
+                    query += $" AND AGE = {ageCondition}";
+
+                if (!string.IsNullOrEmpty(nameCondition))
+                    query += $" AND NAME LIKE '%{nameCondition}%'";
+
+                if (!string.IsNullOrEmpty(locationCondition))
+                    query += $" AND LOCATION LIKE '%{locationCondition}%'";
+
+                // Create a SqlCommand object
+                using (SqlCommand cmd = new SqlCommand(query, con))
+                {
+                    // Create a DataTable to store the retrieved data
+                    System.Data.DataTable dataTable = new System.Data.DataTable();
+
+                    // Create a SqlDataAdapter to fill the DataTable
+                    using (SqlDataAdapter adapter = new SqlDataAdapter(cmd))
+                    {
+                        // Fill the DataTable with the data from the SqlDataAdapter
+                        adapter.Fill(dataTable);
+                    }
+
+                    // Display the data in the DataGridView
+                    if (dataTable.Rows.Count > 0)
+                    {
+                        dataGridView1.DataSource = dataTable;
+                    }
+                    else
+                    {
+                        // Display a message indicating no data found
+                        MessageBox.Show("No data found for the specified criteria.", "Data Not Found", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                // Close the database connection
+                con.Close();
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+
+
+    
+
